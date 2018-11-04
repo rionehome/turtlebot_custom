af83662c44f97f54ca4028de586eb4404eae3807
Nenetti <akinori1090@gmail.com>
2018-11-04 15:52:49 +0900

    gazebo対応

--- patches/copy.sh
+++ patches/copy.sh
@@ -4,5 +4,6 @@ DIR=$(cd $(dirname $0); pwd)
 DEST=$(rospack find turtlebot_description)/urdf
 
 sudo cp -f $DIR/turtlebot_common_library.urdf.xacro ${DEST}/turtlebot_common_library.urdf.xacro
-
-echo -e "\n complete copy turtlebot.urdf.xacro \n"
+sudo cp -f $DIR/turtlebot_gazebo.urdf.xacro ${DEST}/turtlebot_gazebo.urdf.xacro
+echo -e "\n complete copy turtlebot_common_library.urdf.xacro \n"
+echo -e "\n complete copy turtlebot_gazebo.urdf.xacro \n"
