b00078b6222dcf56ca8448c2850ab6ece23900ef
Nenetti <akinori1090@gmail.com>
2018-11-04 15:08:18 +0900

    0.2

--- patches/copy.sh
+++ patches/copy.sh
@@ -3,6 +3,6 @@
 DIR=$(cd $(dirname $0); pwd)
 DEST=$(rospack find turtlebot_description)/urdf
 
-cp -f ${DEST}/turtlebot_common_library.urdf.xacro $DIR/turtlebot_common_library.urdf.xacro
+sudo cp -f $DIR/turtlebot_common_library.urdf.xacro ${DEST}/turtlebot_common_library.urdf.xacro
 
 echo -e "\n complete copy turtlebot.urdf.xacro \n"
