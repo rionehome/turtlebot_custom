1258d48555c07418812b85a35cccad8197bf7af8
Nenetti <36143239+Nenetti@users.noreply.github.com>
2018-12-05 22:56:21 +0900

    Update copy.sh

--- patches/copy.sh
+++ patches/copy.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-DIR=$(cd $(dirname $0); pwd)
-DEST=$(rospack find turtlebot_description)/urdf
+#DIR=$(cd $(dirname $0); pwd)
+#DEST=$(rospack find turtlebot_description)/urdf
 
-sudo cp -f $DIR/turtlebot_common_library.urdf.xacro ${DEST}/turtlebot_common_library.urdf.xacro
-sudo cp -f $DIR/turtlebot_gazebo.urdf.xacro ${DEST}/turtlebot_gazebo.urdf.xacro
-echo -e "\n complete copy turtlebot_common_library.urdf.xacro \n"
-echo -e "\n complete copy turtlebot_gazebo.urdf.xacro \n"
+#sudo cp -f $DIR/turtlebot_common_library.urdf.xacro ${DEST}/turtlebot_common_library.urdf.xacro
+#sudo cp -f $DIR/turtlebot_gazebo.urdf.xacro ${DEST}/turtlebot_gazebo.urdf.xacro
+#echo -e "\n complete copy turtlebot_common_library.urdf.xacro \n"
+#echo -e "\n complete copy turtlebot_gazebo.urdf.xacro \n"
